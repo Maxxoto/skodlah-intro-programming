1a5f38670c9964064f1e588b5ae08e548eee569c
Dani Forest <danisetiawan.forest@gmail.com>
2023-04-03 09:10:45 +0000

    add materi

---
+++ variabel.js
@@ -0,0 +1,24 @@
+var kalimat = "abc"; // String
+var harga = 5000; // Number
+var list_pakaian = ["baju", "celana"]; // Array Object
+var hari_ini = new Date(); // Date
+var mobil = {
+  merk: "avanxa",
+  roda: 4,
+};
+// Object
+var x = true; //Boolean
+var y; // Undefined
+var z = null; //Null
+
+// console.log(typeof hari_ini);
+// console.log(Array.isArray(list_pakaian));
+// console.log(hari_ini.constructor.name);
+// console.log(list_pakaian.constructor.name);
+// console.log(mobil.constructor.name);
+// tambah(1, 5);
+
+// console.log(typeof z);
+
+// String,Number (interger),Object,Boolean,Undefined,Null
+// Object : Array,Object,Date
